5e2ffcedc3889d4d01543d8f88efaef70f3c303f
marianotob <sr.rizado@gmail.com>
2017-05-22 20:55:15 +0200

    centrado NPCs

--- Psychedelic Night/NPC.cpp
+++ Psychedelic Night/NPC.cpp
@@ -248,8 +248,8 @@ void NPC::movTopo(Time tiempo, int cont_vueltas){
         }
 
         if(retraso==6){
-           x_topo = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/350));
-            y_topo = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/180));
+           x_topo = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/340));
+            y_topo = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/170));
             float signox =  static_cast <float> (rand()) / (static_cast <float> (RAND_MAX));
             float signoy =  static_cast <float> (rand()) / (static_cast <float> (RAND_MAX));
             if (signox >= 0.5)
